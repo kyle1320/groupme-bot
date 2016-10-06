cc47daa7e78aaa1dfa32db058b31e8e27f30cd87
Kyle Cutler <kyle1320@gmail.com>
2016-10-05 22:42:29 -0400

    make help arguments caseless

--- bots/hogs/commands/help.js
+++ bots/hogs/commands/help.js
@@ -10,7 +10,7 @@ module.exports = function (args) {
 
         // remove leading ! or /
         if (/^[!\/]/.test(cmd)) {
-            cmd = cmd.substring(1);
+            cmd = cmd.substring(1).toLowerCase();
         }
 
         if (cmd in commands) {
