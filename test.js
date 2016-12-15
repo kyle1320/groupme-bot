8b1e46e49c11f6f233785d270688653f78b76c00
Kyle Cutler <kyle1320@gmail.com>
2016-12-14 22:21:59 -0500

    update testing suite to reflect changes in bot structure

--- test.js
+++ test.js
@@ -1,87 +1,59 @@
-var assert = require('assert');
+'use strict';
 
-var BotRunner = require('./botrunner');
-var harambe = new (require('./bots/harambe'))('harambeid');
-var artkalb = new (require('./bots/artkalb'))('artkalbid', 1500, [
-    /blahblah\b/gi, "BlahBlahBlah"
-]);
-
-function checkMessages(msgs, ...contents) {
-    assert.equal(msgs.length, contents.length);
-
-    assert(contents.every(function (content) {
-        var index = msgs.findIndex(function (msg) {
-            return msg.text === content.text;
-        });
-
-        if (index >= 0) {
-            var msg = msgs.splice(index, 1)[0];
-
-            for (var key in content) {
-                assert.equal(msg[key], content[key]);
-            }
-        }
-
-        return index >= 0;
-    }));
-}
-
-// test Harambe bot
-
-var harambeTest1 = harambe.consult({text: 'harambe'});
-assert(harambeTest1);
-assert.equal(harambeTest1.length, 1);
-assert.equal(harambeTest1[0].text, 'Dicks out for Harambe!');
-
-var harambeTest2 = harambe.consult({text: 'cHaraMber alwhna'});
-assert(harambeTest2);
-assert.equal(harambeTest2.length, 1);
-assert.equal(harambeTest2[0].text, 'Dicks out for Harambe!');
-
-var harambeTest3 = harambe.consult({text: 'nothing :('});
-assert.equal(harambeTest3.length, 0);
-
-var harambeTest4 = harambe.consult({text: 'nothing', name: 'I am harambe!'});
-assert(harambeTest4);
-assert.equal(harambeTest4.length, 1);
-assert.equal(harambeTest4[0].text, 'Harambe lives!');
-
-var harambeTest5 = harambe.consult({text: 'aaaharambea', name: 'I am harambe!'});
-assert(harambeTest5);
-checkMessages(harambeTest5,
-    {text: 'Harambe lives!'},
-    {text: 'Dicks out for Harambe!'}
-);
-
-// test Art Kalb bot
-
-var artkalbTest1 = artkalb.consult({text: 'nothing :('});
-assert(!artkalbTest1);
-
-var artkalbTest2 = artkalb.consult({text: 'abcer'});
-assert(!artkalbTest2);
-
-var artkalbTest3 = artkalb.consult({text: 'NaIler wiper'});
-assert(artkalbTest3);
-assert.equal(artkalbTest3.text, 'NaIler? I hardly know her!');
-
-var artkalbTest4 = artkalb.consult({text: 'creater'});
-assert(!artkalbTest4);
-
-// test Bot Runner in verbose mode
-
-var testBotRunner = new BotRunner(function(){}, {
-    verbose: true,
-    debugBotId: 'debugid'
-});
-testBotRunner.addBot(artkalb);
-testBotRunner.addBot(harambe);
-testBotRunner.listen(3000);
-
+const assert = require('assert');
+const BotRunner = require('./botrunner');
 const http = require('http');
 
-function testRequest(path, body) {
-    console.log("TEST", path, body);
+process.env.ARTKALB_MESSAGE_DELAY = 1500;
+process.env.ARTKALB_SPECIAL_CASES = '["blahblah\\\\b", "BlahBlahBlah"]';
+
+var messages = [];
+var harambe = new (require('./bots/harambe'))('harambeid', pushMessage);
+var artkalb = new (require('./bots/artkalb'))('artkalbid', pushMessage);
+var hogs = new (require('./bots/hogs'))('hogsid', pushMessage);
+var debug = new (require ('./bots/debug'))('debugid', pushMessage);
+
+var testBotRunner = new BotRunner();
+testBotRunner.addBot(artkalb);
+testBotRunner.addBot(harambe);
+testBotRunner.addBot(hogs);
+testBotRunner.addBot(debug);
+
+function pushMessage(msg) {
+    messages.push(msg);
+}
+
+function checkMessages(...contents) {
+    assert.equal(messages.length, contents.length, "incorrect number of messages");
+
+    contents.forEach(function (content) {
+        var index = messages.findIndex(function (msg) {
+            for (var key in content) {
+                if (content[key] instanceof RegExp) {
+                    if (!content[key].test(msg[key])) {
+                        return false;
+                    }
+                } else if (msg[key] !== content[key]) {
+                    return false;
+                }
+            }
+            return true;
+        });
+
+        assert(index >= 0, "message not found: " + JSON.stringify(content));
+
+        messages.splice(index, 1);
+    });
+}
+
+function testBot(bot, body, ...responses) {
+    console.log("TEST", bot.name, body);
+    bot.consult(body);
+    checkMessages.apply(null, responses);
+}
+
+function testRequest(path, body, ...responses) {
+    console.log("POST", path, body);
     return new Promise(function(resolve, reject) {
         var arr = [];
         testBotRunner.submit = function(msg) {
@@ -97,92 +69,140 @@ function testRequest(path, body) {
             }
         }).end(JSON.stringify(body));
         setTimeout(function() {resolve(arr)}, 1000);
+    }).then(function () {
+        checkMessages.apply(null, responses);
     });
 }
 
+// test Harambe bot
+
+console.log('====================\nRunning local tests...\n====================');
+
+testBot(
+    harambe,
+    {text: 'harambe'},
+    {text: 'Dicks out for Harambe!'}
+);
+
+testBot(
+    harambe,
+    {text: 'cHaraMber alwhna'},
+    {text: 'Dicks out for Harambe!'}
+);
+
+testBot(
+    harambe,
+    {text: 'nothing :('}
+);
+
+testBot(
+    harambe,
+    {text: 'nothing', name: 'I am harambe!'},
+    {text: 'Harambe lives!'}
+);
+
+testBot(
+    harambe,
+    {text: 'aaaharambea', name: 'I am harambe!'},
+    {text: 'Harambe lives!'},
+    {text: 'Dicks out for Harambe!'}
+);
+
+// test Art Kalb bot
+
+testBot(
+    artkalb,
+    {text: 'nothing :('}
+);
+
+testBot(
+    artkalb,
+    {text: 'abcer'}
+);
+
+testBot(
+    artkalb,
+    {text: 'NaIler wiper'},
+    {text: 'NaIler? I hardly know her!'}
+);
+
+testBot(
+    artkalb,
+    {text: 'creater'}
+);
+
+// test hogs bot
+
+testBot(
+    hogs,
+    {text: '/help'},
+    {text: /.+/}
+);
+
+testBot(
+    hogs,
+    {text: '/test'},
+    {text: '>This is a test\nThis is a test'}
+);
+
+testBot(
+    hogs,
+    {text: '/eval 1<<10'},
+    {text: '>>>> 1<<10\n1024'}
+);
+
+// test debug bot
+
+testBot(
+    debug,
+    {text: 'harambe'},
+    {text: 'harambe: Dicks out for Harambe!'}
+);
+
+testBot(
+    debug,
+    {text: '/help harambe'},
+    {text: 'harambe: Dicks out for Harambe!'},
+    {text: /^hogs\: /}
+);
+
+// test Bot Runner
+
+console.log('====================\nRunning http tests...\n====================');
+
+var testServer = testBotRunner.app.listen(3000);
+
 testRequest(
     '/artkalb',
-    {'text': 'hello'}
-).then(function(msgs) {
-    assert.equal(msgs.length, 0);
-}).then(function() {
+    {text: 'hello'}
+).then(function() {
     return testRequest(
         '/harambe',
-        {'text': 'harambe'}
+        {text: 'harambe'},
+        {bot_id: 'harambeid', text: 'Dicks out for Harambe!'}
     );
-}).then(function(msgs) {
-    assert.equal(msgs.length, 1);
-    assert.equal(msgs[0].text, 'Dicks out for Harambe!');
-    assert.equal(msgs[0].bot_id, 'harambeid');
 }).then(function() {
     return testRequest(
-        '/all',
-        {'text': 'harambe translator'}
-    );
-}).then(function(msgs) {
-    checkMessages(msgs,
-        {text: 'Dicks out for Harambe!'},
-        {text: 'translator? I hardly know her!'}
+        '/debug',
+        {text: '/help harambe translator'},
+        {bot_id: 'debugid', text: 'harambe: Dicks out for Harambe!'},
+        {bot_id: 'debugid', text: 'artkalb: translator? I hardly know her!'},
+        {bot_id: 'debugid', text: /^hogs\: /}
     );
 }).then(function() {
-    testBotRunner.close();
-
-    // test Bot Runner in non-verbose mode
-
-    testBotRunner = new BotRunner(function(){}, {
-        verbose: false,
-        debugBotId: 'debugid'
-    });
-    testBotRunner.addBot(artkalb);
-    testBotRunner.addBot(harambe);
-    testBotRunner.listen(3000);
-
     return testRequest(
         '/artkalb',
-        {'text': 'hello'}
-    )
-}).then(function(msgs) {
-    assert.equal(msgs.length, 0);
-}).then(function() {
-    return testRequest(
-        '/harambe',
-        {'text': 'harambe'}
-    );
-}).then(function(msgs) {
-    assert.equal(msgs.length, 1);
-    assert.equal(msgs[0].text, 'Dicks out for Harambe!');
-    assert.equal(msgs[0].bot_id, 'harambeid');
-}).then(function() {
-    return testRequest(
-        '/all?debug',
-        {'text': 'harambe translator', 'name': 'harambee'}
-    );
-}).then(function(msgs) {
-    checkMessages(msgs,
-        {text: 'harambe: Dicks out for Harambe!', bot_id: 'debugid'},
-        {text: 'harambe: Harambe lives!', bot_id: 'debugid'},
-        {text: 'artkalb: translator? I hardly know her!', bot_id: 'debugid'}
+        {text: 'blahblah vibrator'},
+        {bot_id: 'artkalbid', text: 'BlahBlahBlah? I hardly know her!'}
     );
 }).then(function() {
     return testRequest(
-        '/artkalb?debug',
-        {'text': 'blahblah vibrator'}
+        '/hogs',
+        {text: '/eval 5+5'},
+        {bot_id: 'hogsid', text: '>>>> 5+5\n10'}
     );
-}).then(function(msgs) {
-    assert.equal(msgs.length, 1);
-    assert.equal(msgs[0].text, 'artkalb: BlahBlahBlah? I hardly know her!');
-    assert.equal(msgs[0].bot_id, 'debugid');
 }).then(function() {
-    return testRequest(
-        '/harambe?debug',
-        {'text': 'harambe'}
-    );
-}).then(function(msgs) {
-    assert.equal(msgs.length, 1);
-    assert.equal(msgs[0].text, 'harambe: Dicks out for Harambe!');
-    assert.equal(msgs[0].bot_id, 'debugid');
-}).then(function() {
-    testBotRunner.close();
+    testServer.close();
 
     console.log('passed all tests');
 }).catch(function (err) {
