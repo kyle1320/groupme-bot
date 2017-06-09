caa20d6a499c89c5db59293d49239b4bf807a865
Kyle Cutler <kyle1320@gmail.com>
2017-06-09 15:36:49 -0600

    add link to RIT ResLife

--- bots/hogs/commands/links.js
+++ bots/hogs/commands/links.js
@@ -4,7 +4,8 @@ module.exports = function (args) {
     return `HOGS Website: https://hogs.rit.edu/
 Pawprints: https://pawprints.rit.edu/
 Tiger Center: https://tigercenter.rit.edu/
-RIT Reddit: https://www.reddit.com/r/rit/`;
+RIT Reddit: https://www.reddit.com/r/rit/
+ResLife: https://www.rit.edu/studentaffairs/reslife/`;
 };
 
 module.exports.helpString =
