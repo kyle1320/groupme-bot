9fb1bef42ff0f7dbb5082f21f2d6ea995c3532ea
Kyle Cutler <kyle1320@gmail.com>
2017-06-09 16:36:32 -0600

    recreate context every time, add range and sum functions

--- bots/hogs/commands/calc.js
+++ bots/hogs/commands/calc.js
@@ -7,7 +7,7 @@ module.exports = function (args) {
     var val;
 
     try {
-        val = String(vm.runInNewContext(code, Math, {timeout: 100}))
+        val = String(vm.runInNewContext(code, getContext(), {timeout: 100}))
     } catch (e) {
         val = e;
     }
@@ -17,3 +17,49 @@ module.exports = function (args) {
 
 module.exports.helpString =
 `Evaluates the given text as JavaScript, and prints the result.`;
+
+function getContext() {
+    const context = Object.create(Math);
+
+    context.range = function*(arg0 = Infinity, arg1, arg2) {
+        var min, max, skip;
+
+        if (typeof arg1 !== 'number') {
+            min = 0;
+            max = arg0;
+        } else {
+            min = arg0;
+            max = arg1;
+        }
+
+        if (typeof arg2 !== 'number') skip = Math.sign(max - min);
+        else                          skip = arg2
+
+        const direction = Math.sign(skip);
+
+        while (Math.sign(max - min) === direction) {
+            yield min;
+            min += skip;
+        }
+    };
+
+    context.sum = function(...values) {
+        var sum = 0;
+        for (var item of flatmap(values)) sum += item;
+        return sum;
+    };
+
+    function isIterable(obj) {
+        if (obj == null) return false;
+        return typeof obj[Symbol.iterator] === 'function';
+    }
+
+    function* flatmap(values) {
+        for (var item of values) {
+            if (isIterable(item)) yield* item;
+            else                  yield  item;
+        }
+    }
+
+    return context;
+}
